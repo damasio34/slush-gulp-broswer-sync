e426cde4481fc906f6c9e8568f79c04f29aa100a
Darlan Damasio <damasio34@gmail.com>
2016-09-22 01:35:32 -0300

    Simplificação

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,45 +1,10 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var imagemin    = require('gulp-imagemin');
-var uglify     	= require('gulp-uglify');
-var concat 		= require('gulp-concat');
-var del 		= require('del');
-
-var paths = {
-  scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
-  images: 'client/img/**/*'
-};
-
-gulp.task('clean', function(cb) {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  del(['build'], cb);
-});
-
-// Copy all static images
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-});
-
-gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
-  return gulp.src(paths.scripts)
-    .pipe(sourcemaps.init())
-      .pipe(coffee())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/js'));
-});
 
 // process JS files and return the stream.
 gulp.task('js', function () {
     return gulp.src('app/js/*js')
-        .pipe(browserify())
-        .pipe(uglify());
+        .pipe(browserify());
 });
 
 // create a task that ensures the `js` task is complete before
